d876bff6a53e43787b8000010cd8672012a6c24b
Vlad <66175053+VladHZC@users.noreply.github.com>
2021-01-11 20:46:51 -0800

    Delete setup.py

--- setup.py
+++
@@ -1,8 +0,0 @@
-from cx_Freeze import setup, Executable
-
-setup(
-    name = "LBRY TOOLS",
-    version = "0.1",
-    description = "",
-    executables = [Executable("install.py", base="Win32GUI")],
-)
